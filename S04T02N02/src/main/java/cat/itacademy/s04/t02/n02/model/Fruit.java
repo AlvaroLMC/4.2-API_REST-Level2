f240b40a0efedcf98d2d000456e23f23080c78b9
Alvaro Mendoza <alvaromendoxxa@gmail.com>
2025-07-26 19:55:22 +0200

    incorporation akarta validation

--- S04T02N02/src/main/java/cat/itacademy/s04/t02/n02/model/Fruit.java
+++ S04T02N02/src/main/java/cat/itacademy/s04/t02/n02/model/Fruit.java
@@ -1,11 +1,13 @@
 package cat.itacademy.s04.t02.n02.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "fruit")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,6 +17,10 @@ public class Fruit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotBlank(message = "Name is mandatory")
+    @Size(max = 50, message = "Name must be at most 50 characters")
     private String name;
+
+    @Min(value = 0, message = "Quantity must be zero or positive")
     private int quantityKilos;
 }
